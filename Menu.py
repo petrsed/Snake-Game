c307f293ed8bf3d61bed33f862bbdb3e632b85ca
PetrSed <45389741+PetrSed@users.noreply.github.com>
2019-12-21 02:05:58 +0700

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -70,3 +70,38 @@ class PauseMenu:
     def terminate(self):
         pygame.quit()
         sys.exit()
+
+
+class LoseMenu:
+    def __init__(self, size):
+        self.screen_size = size
+        self.x, self.y = self.screen_size
+        self.menu_screen = pygame.display.set_mode(self.screen_size)
+        self.menu_background_image = pygame.image.load('data/lose_background.png').convert()
+        self.clock = pygame.time.Clock()
+        pygame.display.update()
+
+    def start(self):
+        self.pause_on = True
+        while self.pause_on:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.get_click(event.pos)
+            self.menu_screen.blit(self.menu_background_image, (0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+
+    def get_click(self, coords):
+        x, y = coords
+        if x >= 304 and x <= 690 and y >= 461 and y <= 504:
+            new_game = Game(self.screen_size)
+            new_game.start()
+        elif x >= 397 and x <= 600 and y >= 517 and y <= 559:
+            menu = MainMenu(self.screen_size)
+            menu.start()
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
