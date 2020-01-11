744c3ac1938318cb5b0b7768c1eb735c211b0bed
sLegionv <54397351+sLegionv@users.noreply.github.com>
2020-01-12 00:04:25 +0700

    fdf
    fdf

--- Snake.py
+++ Snake.py
@@ -1,5 +1,13 @@
 import pygame
 import random
+import os
+from random import choice
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    return image
 
 
 class Snake:
@@ -12,24 +20,21 @@ class Snake:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.snake_size = 20
-        self.snake_step = 10
+        self.snake_step = 20
         self.snake_head_pos = [100, 50]
-        self.snake_body_pos = [[90, 50], [80, 50]]
-        self.direction = self.up
-        self.change_direction = self.direction
+        self.snake_body_pos = [[value, 50] for value in range(100, 0, -30)]
+        self.direction = self.right
 
-    def check_direction(self):
-        if (self.change_direction == self.right and not self.direction == self.left) or\
-                (self.change_direction == self.left and not self.direction == self.right) or\
-                (self.change_direction == self.up and not self.direction == self.down) or\
-                (self.change_direction == self.down and not self.direction == self.up):
-            self.direction = self.change_direction
+    def check_direction(self, change_direction):
+        if (change_direction == self.right and not self.direction == self.left) or\
+                (change_direction == self.left and not self.direction == self.right) or\
+                (change_direction == self.up and not self.direction == self.down) or\
+                (change_direction == self.down and not self.direction == self.up):
+            self.direction = change_direction
             return True
         return False
 
     def change_head_pos(self):
-        if not self.check_direction():
-            return
         if self.direction == self.right:
             self.snake_head_pos[0] += self.snake_step
         elif self.direction == self.left:
@@ -39,42 +44,54 @@ class Snake:
         elif self.direction == self.down:
             self.snake_head_pos[1] += self.snake_step
 
-    def check_snake(self, score, food_pos):
-        self.snake_body_pos.insert(0, self.snake_head_pos)
-        if self.snake_head_pos == food_pos:
-            score += 1
-        else:
-            self.snake_body_pos.pop()
-        return score
+    def check_snake(self, food_pos, food_size):
+        self.snake_body_pos.insert(0, list(self.snake_head_pos))
+        x_snake_1, y_snake_1 = self.snake_head_pos
+        x_snake_2, y_snake_2 = x_snake_1 + self.snake_size, y_snake_1 + self.snake_size
+        x_food_1, y_food_1 = food_pos
+        x_food_2, y_food_2 = x_food_1 + food_size, y_food_1 + food_size
+        if x_food_1 <= x_snake_2 <= x_food_2 and y_food_1 <= y_snake_2 <= y_food_2:
+            return True
+        self.snake_body_pos.pop()
+        return False
 
-    def draw_snake(self, surface, game):
-        game.draw_map()  # Прорисовка карты
+    def draw_snake(self, surface):
         for pos in self.snake_body_pos:
-            pygame.draw.rect(surface, pygame.Color('blue'), pygame.Rect(*pos, self.snake_size, self.snake_size))
+            surface.fill(pygame.Color('blue'), pygame.Rect(pos, (self.snake_size, self.snake_size)))
 
     def check_collision(self, game):
         if self.check_boundaries() or self.check_eating_oneself():
-            game.game_over()  # game - экземпляр класса Game
+            game.game_over()
 
     def check_boundaries(self):
         return (self.snake_head_pos[0] > self.screen_width or self.snake_head_pos[0] < 0)\
                or (self.snake_head_pos[1] > self.screen_height or self.snake_head_pos[1] < 0)
 
     def check_eating_oneself(self):
-        for block in self.snake_body_pos:
+        for block in self.snake_body_pos[1:]:
             return block[0] == self.snake_head_pos[0] and block[1] == self.snake_head_pos[1]
 
 
-class Food:
-    def __init__(self, food_image, screen_width, screen_height):
-        self.food_image = food_image
-        self.food_size = 10
-        self.food_pos = [random.randrange(1, screen_width // self.food_size) * self.food_size,
-                         random.randrange(1, screen_height // self.food_size) * self.food_size]
+class Food(pygame.sprite.Sprite):
+    def __init__(self, screen_width, screen_height):
+        super().__init__()
+        self.images = [load_image(el) for el in ['apple.png', 'banana.png', 'cherry.png']]
+        self.image = None
+        self.rect = None
+        self.size = 30, 30
+        self.screen_width, self.screen_height = screen_width, screen_height
+        self.update()
 
-    def draw_food(self, surface):
-        x, y = self.food_pos
-        pygame.draw.rect(surface, pygame.Color('red'), pygame.Rect(x, y, self.food_size, self.food_size))
+    def update(self):
+        self.set_image()
+        self.rect.x = random.randrange(1, self.screen_width // 10) * 10
+        self.rect.y = random.randrange(1, self.screen_height // 10) * 10
 
-    def set_food_pos(self, pos):
-        self.food_pos = pos
+    def set_image(self):
+        self.image = pygame.transform.scale(choice(self.images), self.size)
+        self.rect = self.image.get_rect()
+
+    def get_pos(self):
+        return self.rect.x, self.rect.y
+
+
